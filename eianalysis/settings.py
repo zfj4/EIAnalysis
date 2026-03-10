cf648f63ba741d5726c8a454fd2d86afd2efad6b
John Copeland <zfj4@cdc.gov>
2026-03-10 13:12:51 -0400

    Made cookies unique to the application

--- eianalysis/settings.py
+++ eianalysis/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Unique cookie names to avoid conflicts with other Django apps on 127.0.0.1
+CSRF_COOKIE_NAME = 'eianalysis_csrftoken'
+SESSION_COOKIE_NAME = 'eianalysis_sessionid'
