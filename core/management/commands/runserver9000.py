5f962163ffbb1c6a51d7f02d7acde3f833f7177f
John Copeland <zfj4@cdc.gov>
2026-03-13 13:46:42 -0400

    Added "Powered by epiinfo" to the banner

--- core/management/commands/runserver9000.py
+++ core/management/commands/runserver9000.py
@@ -4,7 +4,7 @@ Custom management command: `python manage.py runserver9000`
 Starts the development server on 127.0.0.1:9000 by default so that
 `python manage.py runserver` on port 8000 (occupied) is never used accidentally.
 """
-from django.core.management.commands.runserver import Command as RunserverCommand
+from django.contrib.staticfiles.management.commands.runserver import Command as RunserverCommand
 
 
 class Command(RunserverCommand):
